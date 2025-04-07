31cd9e0b7f32189507bc36c5695996ab5aeefb57
CuprumBuddy <166859266+CuprumBuddy@users.noreply.github.com>
2025-04-07 13:40:56 +0300

    Turned on comments

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+        provider: 'giscus',
+        options: {
+          // from data-repo
+          repo: 'CuprumBuddy/cuprum-garden',
+          // from data-repo-id
+          repoId: 'R_kgDOOO5umg',
+          // from data-category
+          category: 'Announcements',
+          // from data-category-id
+          categoryId: 'DIC_kwDOOO5ums4Co26W',
+    }
+  }),
+],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
